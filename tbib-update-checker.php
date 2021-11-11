7a9546a1353287bee4d845380a4deceba13b8bc0
the-best-is-best <michelle.raouf@outlook.com>
2021-11-11 13:03:33 +0200

    init

---
+++ tbib-update-checker.php
@@ -0,0 +1,11 @@
+<?php
+/**
+
+ * Plugin Name:       TBIB Update Checker
+ * Description:       Update TBIB Plugins.
+ * Version:           1.0
+ * Author:            Michelle Raouf - TBIB
+
+ * **/
+
+require dirname(__FILE__) . '/load-v4p11.php';
