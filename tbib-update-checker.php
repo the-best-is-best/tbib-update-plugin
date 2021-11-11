1349541ea8fa036ecab27b876826c9cd9a54f2ee
the-best-is-best <michelle.raouf@outlook.com>
2021-11-11 14:26:55 +0200

    support auto update

--- tbib-update-checker.php
+++ tbib-update-checker.php
@@ -1,11 +1,18 @@
 <?php
 /**
-
  * Plugin Name:       TBIB Update Checker
  * Description:       Update TBIB Plugins.
- * Version:           1.0
+ * Version:           1.1
  * Author:            Michelle Raouf - TBIB
-
  * **/
 
-require dirname(__FILE__) . '/load-v4p11.php';
+
+    require dirname(__FILE__) . '/load-v4p11.php';
+   
+
+    require_once ABSPATH . '/wp-content/plugins/tbib-update-plugin/tbib-update-checker.php';
+    $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+        'https://raw.githubusercontent.com/the-best-is-best/tbib-polylang-acf/master/check.json',
+        __FILE__, //Full path to the main plugin file or functions.php.
+        'tbib-update-checker'
+    );
